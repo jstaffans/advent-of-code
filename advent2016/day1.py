0da2e383cacf42c65cfb40aca1a4f653d89f741f
Johannes Staffans <johannes.staffans@gmail.com>
2017-11-27 22:22:09 +0100

    part two

--- advent2016/day1.py
+++ advent2016/day1.py
@@ -6,19 +6,42 @@ def moves(s):
     "Parse moves from input string."
     dirs = [N, W, S, E]  # start facing north
     parts = s.split(", ")
-    result = []
+    position = (0+0j)
 
     for p in parts:
         direction = p[0]
         distance = int(p[1:])
         dirs = rotate_left(dirs) if direction == 'L' else rotate_right(dirs)
-        result.append(distance * dirs[0])
+        position += distance * dirs[0]
 
-    return result
+    return position
 
-def distance(ms):
-    all_moves = sum(ms)
-    return abs(all_moves.imag) + abs(all_moves.real)
+def distance(position):
+    return abs(position.imag) + abs(position.real)
+
+
+# part two: first place visited twice
+
+def first_visited_twice(s):
+    dirs = [N, W, S, E]  # start facing north
+    parts = s.split(", ")
+    total = []
+    position = (0+0j)
+    visited = {}
+
+    for p in parts:
+        direction = p[0]
+        distance = int(p[1:])
+        dirs = rotate_left(dirs) if direction == 'L' else rotate_right(dirs)
+        for i in range(distance):
+            visited[position] = True
+            position += dirs[0]
+            if visited.get(position, False):
+                return position
+
+    return position
+
 
 distance(moves(input(2016, 1).read()))
+distance(first_visited_twice(input(2016, 1).read()))
 
