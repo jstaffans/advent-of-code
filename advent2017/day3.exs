94536fb30440487432ee9b612ec75719344272d7
Johannes Staffans <johannes.staffans@gmail.com>
2017-12-03 20:23:10 +0100

    Part two

--- advent2017/day3.exs
+++ advent2017/day3.exs
@@ -9,18 +9,7 @@ defmodule AdventOfCode.Day3 do
   end
 
   defp spiral(position, steps, loop_index) do
-    full_circle =
-      [
-        [Complex.new(1, 0), Complex.new(0, 1)],
-        [Complex.new(-1, 0), Complex.new(-1, 0)],
-        [Complex.new(0, -1), Complex.new(0, -1)],
-        [Complex.new(1, 0), Complex.new(1, 0)]
-      ]
-
-    current_circle =
-      full_circle
-      |> extend_circle(loop_index)
-      |> List.flatten
+    current_circle = circle(loop_index)
 
     {position, steps} = spiral_steps(position, steps, current_circle)
 
@@ -41,17 +30,73 @@ defmodule AdventOfCode.Day3 do
     spiral_steps(Complex.add(position, next_step), steps - 1, rest)
   end
 
-  # Extend the individual vector arrays representing the "sides" of the spiral for the given loop index/layer.
-  defp extend_circle(circle_vectors, loop_index) do
-    Enum.map(circle_vectors, fn (vectors) ->
+  # Get the "circle" of vector arrays for the current loop index (layer)
+  defp circle(loop_index) do
+    full_circle =
+      [
+        [Complex.new(1, 0), Complex.new(0, 1)],
+        [Complex.new(-1, 0), Complex.new(-1, 0)],
+        [Complex.new(0, -1), Complex.new(0, -1)],
+        [Complex.new(1, 0), Complex.new(1, 0)]
+      ]
+
+    full_circle
+    |> Enum.map(fn (vectors) ->
       vectors ++ replicate(loop_index * 2, List.last(vectors))
     end)
+    |> List.flatten
   end
 
   # Calculate Manhattan distance given a position in the complex plane.
   defp calculate_distance(position) do
-    IO.inspect(position)
     abs(Complex.real(position)) + abs(Complex.imaginary(position))
+  end
+
+  # Part Two: first sum of all neighbors that is greater than input.
+  def first_sum_gt(input) do
+    origin = Complex.new(0, 0)
+    lookup = Map.put(%{}, lookup_key(origin), 1)
+    first_sum(origin, fn (x) -> x > input end, lookup, 0)
+  end
+
+  defp lookup_key(position) do
+    {Complex.real(position), Complex.imaginary(position)}
+  end
+
+  # return the first sum for which pred is true
+  defp first_sum(position, pred, lookup, loop_index) do
+    current_circle = circle(loop_index)
+    {position, lookup, last_sum} = spiral_sum(position, pred, lookup, current_circle)
+    if pred.(last_sum), do: last_sum, else: first_sum(position, pred, lookup, loop_index + 1)
+  end
+
+  # given a lookup table, calculate the sum for a given position
+  defp sum_at(position, lookup) do
+    neighbor_vectors = [
+      Complex.new(-1, 1), Complex.new(0, 1), Complex.new(1, 1),
+      Complex.new(-1, 0), Complex.new(1, 0),
+      Complex.new(-1, -1), Complex.new(0, -1), Complex.new(1, -1)
+    ]
+
+    Enum.reduce(
+      neighbor_vectors,
+      0,
+      fn (neighbor_vector, acc) ->
+        neighbor = Complex.add(position, neighbor_vector)
+        neighbor_value = Map.get(lookup, lookup_key(neighbor), 0)
+        acc + neighbor_value
+      end)
+  end
+
+  defp spiral_sum(position, pred, lookup, []) do
+    {position, lookup, Map.get(lookup, lookup_key(position))}
+  end
+
+  defp spiral_sum(position, pred, lookup, [next_step|rest]) do
+    next_position = Complex.add(position, next_step)
+    next_sum = sum_at(next_position, lookup)
+    lookup = Map.put(lookup, lookup_key(next_position), next_sum)
+    if pred.(next_sum), do: {nil, nil, next_sum}, else: spiral_sum(next_position, pred, lookup, rest)
   end
 
 end
@@ -64,3 +109,6 @@ IO.puts AdventOfCode.Day3.manhattan_distance 1024
 IO.puts AdventOfCode.Day3.manhattan_distance 289326
 
 IO.puts "Part two"
+
+IO.puts AdventOfCode.Day3.first_sum_gt 100
+IO.puts AdventOfCode.Day3.first_sum_gt 289326
