2d998acb753fd0b3b3648af28755c425a411a2d5
Johannes Staffans <johannes.staffans@relexsolutions.com>
2018-12-16 21:33:43 +0200

    Cleanup day 3: use RecordWildCards

--- advent2018/src/Day03.hs
+++ advent2018/src/Day03.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE Haskell2010, RecordWildCards #-}
 {-# OPTIONS_GHC -fno-warn-incomplete-patterns #-}
 
 module Day03 where
@@ -12,7 +13,7 @@ import qualified Data.Set as Set
 import Text.Regex.PCRE
 
 data Patch =
-  Patch { id :: Int, x :: Int, y :: Int, width :: Int, height :: Int }
+  Patch { id :: Int, x :: Int, y :: Int, w :: Int, h :: Int }
   deriving Show
 
 type Matrix = UArray (Int, Int) Int
@@ -29,7 +30,7 @@ patchBounds :: [Patch] -> (Int, Int)
 patchBounds patches = go patches 0 0
   where
     go [] xb yb = (xb, yb)
-    go ((Patch { x = x, y = y, width = w, height = h}):ps) xb yb = go ps (max xb (x + w)) (max yb (y + h))
+    go ((Patch {..}):ps) xb yb = go ps (max xb (x + w)) (max yb (y + h))
 
 -- claimCell sets a cell value to the patch id when a cell is as of now unclaimed.
 -- If the cell is claimed, set the cell value to the _negative_ patch id.
@@ -40,7 +41,7 @@ claimCell (Patch { id = id }) currentValue = (-id)
 toMatrix :: [Patch] -> Matrix
 toMatrix patches = runSTUArray $ do
   matrix <- newArray ((0, 0), (patchBounds patches)) 0
-  forM_ patches $ \p@Patch { x = x, y = y, width = w, height = h} -> do
+  forM_ patches $ \p@Patch {..} -> do
     forM_ [y..(y+h-1)] $ \i -> do
       forM [x..(x+w-1)] $ \j -> do
         curr <- readArray matrix (j, i)
@@ -52,7 +53,7 @@ part1 matrix = length $ filter (\id -> id < 0) $ elems matrix
 
 part2 :: [Patch] -> Matrix -> Set.Set (Int, Int)
 part2 patches matrix =
-  let patchSizes = map (\Patch{id = id, width = w, height = h} -> (id, w * h)) patches
+  let patchSizes = map (\Patch{..} -> (id, w * h)) patches
       cells = filter (\id -> id > 0) $ elems matrix
       groupedClaims = map (\ids -> (head ids, length ids)) $ group $ sort cells
   in Set.fromList patchSizes `Set.intersection` Set.fromList groupedClaims
