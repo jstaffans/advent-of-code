0ba83f49cb77642b655c3ba62a234767b009b254
Johannes Staffans <johannes.staffans@relexsolutions.com>
2018-12-17 21:09:12 +0200

    Much more efficient Day 5

--- advent2018/src/Day05.hs
+++ advent2018/src/Day05.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE ViewPatterns, PatternSynonyms #-}
+
 module Day05 where
 
 import Control.Monad
@@ -7,24 +9,40 @@ import Data.Array.Unboxed
 import Data.List
 import Data.List.Split
 import Data.Char
+import qualified Data.Sequence as Seq
+import Data.Foldable (toList)
 
-react :: String -> String
-react s = go s []
+pattern Empty         <- (Seq.viewl -> Seq.EmptyL)
+pattern x :< xs       <- (Seq.viewl -> x Seq.:< xs)
+
+react :: Seq.Seq Char -> Seq.Seq Char
+react s = go s Seq.empty
   where
-    go [] out = out
-    go (c:[]) out = out ++ [c]
-    go (c1:c2:rest) out
-      | c1 /= c2 && toLower c1 == toLower c2 = go rest out
-      | otherwise                           = go ([c2] ++ rest) (out ++ [c1])
+    go Empty out     = out
+    go (x :< xs) out
+      | Seq.length xs == 0 = out Seq.|> x
+      | let x1 = Seq.index xs 0
+        in x /= x1 && toLower x == toLower x1 = go (Seq.drop 1 xs) out
+      | otherwise = go xs (out Seq.|> x)
+
+-- react :: String -> String
+-- react s = go s []
+--   where
+--     go [] out = out
+--     go (c:[]) out = out ++ [c]
+--     go (c1:c2:rest) out
+--       | c1 /= c2 && toLower c1 == toLower c2 = go rest out
+--       | otherwise                           = go ([c2] ++ rest) (out ++ [c1])
 
 chainReact :: String -> String
-chainReact s = go (react s) (length s)
+chainReact s = go (react $ Seq.fromList s) (length s)
   where
     go s origLength
-      | length s == origLength  = s
+      | length s == origLength  = toList s
       | otherwise               = go (react s) (length s)
 
 main :: IO ()
 main = do
   input <- readFile "data/day05.txt"
+  -- print $ toList $ chainReact input
   print $ (length $ chainReact input) - 1
--- advent2018/src/Main.hs
+++ advent2018/src/Main.hs
@@ -5,9 +5,8 @@ module Main where
 import qualified Day01 as Day01
 import qualified Day02 as Day02
 import qualified Day03 as Day03
+import qualified Day05 as Day05
 
 main :: IO ()
 main = do
-  -- Day01.main
-  --Day02.main
-  Day03.main
+  Day05.main
