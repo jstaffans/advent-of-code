482a29323ecc62541d59845a783811c8bd75ee07
Johannes Staffans <johannes.staffans@gmail.com>
2020-12-09 08:14:36 +0200

    Day 8 part 2

--- advent2020/08/aoc08.go
+++ advent2020/08/aoc08.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -18,30 +19,57 @@ func main() {
 	rawLines := strings.Split(txt, "\n")
 	lines := rawLines[:len(rawLines)-1]
 
-	visited := make([]bool, len(lines))
 	acc := 0
 	i := 0
 
-	for {
-		if visited[i] {
-			break
+	lineToSwitch := 0
+	r := regexp.MustCompile("^(nop|jmp).+$")
+
+	updatedLines := make([]string, len(lines))
+
+	switchInstruction := func(s string) string {
+		parts := strings.Split(s, " ")
+		if parts[0] == "nop" {
+			return "jmp " + parts[1]
+		} else {
+			return "nop " + parts[1]
+		}
+	}
+
+	for i < len(lines) {
+		for !r.MatchString(lines[lineToSwitch]) {
+			lineToSwitch++
 		}
 
-		visited[i] = true
+		copy(updatedLines, lines)
+		updatedLines[lineToSwitch] = r.ReplaceAllStringFunc(lines[lineToSwitch], switchInstruction)
 
-		instruction := lines[i]
-		parts := strings.Split(instruction, " ")
-		command, strOffset := parts[0], parts[1]
-		offset, _ := strconv.Atoi(strOffset)
+		visited := make([]bool, len(lines))
+		acc = 0
+		i = 0
 
-		switch command {
-		case "acc":
-			acc += offset
-			i++
-		case "jmp":
-			i += offset
-		case "nop":
-			i++
+		for {
+			if i >= len(lines) || visited[i] {
+				lineToSwitch++
+				break
+			}
+
+			visited[i] = true
+
+			instruction := updatedLines[i]
+			parts := strings.Split(instruction, " ")
+			command, strOffset := parts[0], parts[1]
+			offset, _ := strconv.Atoi(strOffset)
+
+			switch command {
+			case "acc":
+				acc += offset
+				i++
+			case "jmp":
+				i += offset
+			case "nop":
+				i++
+			}
 		}
 	}
 
