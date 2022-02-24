4651d886d93cf4b303c5d91101a01fbab14c5dff
Johannes Staffans <johannes.staffans@gmail.com>
2022-02-24 15:42:18 +0200

    Functional version of part 1

--- advent2021/day01/src/main.rs
+++ advent2021/day01/src/main.rs
@@ -1,8 +1,11 @@
-    use std::fs;
+use std::fs;
+use itertools::Itertools;
 
 fn main() {
     let input = fs::read_to_string("./input.txt").expect("Could not read file");
     let mut depths: Vec<i32> = vec![];
+    
+    // Looping
     for l in input.lines() {
         let depth = l.parse().expect("Could not parse depth");
         depths.push(depth)
@@ -17,7 +20,16 @@ fn main() {
 
     println!("Increases, Part 1: {}", increases);
 
-    increases = 0;
+    // Functional
+    let increases = input
+        .lines()
+        .filter_map(|s| s.parse::<i32>().ok())
+        .tuple_windows()
+        .filter(|(prev, curr)| prev < curr)
+        .count();
+    println!("Increases, Part 1 (second method): {}", increases);
+
+    let mut increases = 0;
 
     for i in 3..depths.len() {
         let prev_window_sum:i32 = depths[i-3..=i-1].iter().sum();
